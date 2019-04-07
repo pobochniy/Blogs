3ccf2514064c9291162d520d6a7e8097cc6abc2b
andrianov.n <andrianov.n@M9-NB-188>
2019-04-07 22:10:47 +0300

    Article.cs

---
+++ Adminka/Models/Article.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adminka.Models
+{
+    public class Article
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Заголовок
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Теги
+        /// </summary>
+        public string Keywords { get; set; }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Содержание
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
